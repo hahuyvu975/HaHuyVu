88291a5af8b2b74f495d77e4840bf27134491d35
unknown <hahuyvu975@gmail.com>
2024-04-09 17:46:14 +0700

    connect router api

--- src/problem5/server.ts
+++ src/problem5/server.ts
@@ -1,12 +1,19 @@
 import dotenv from 'dotenv';
 import express from "express";
+import routes from './routes/routes';
 
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+
+app.use('/api/v1', routes)
+
 app.get("/", (req, res) => {
-  res.send("Hello 213 !");
+  res.send("Hello World!");
 });
+
+
 const port = process.env.PORT
 app.listen(port, () => {
   console.log(`Server app listening on port ${port}`);
