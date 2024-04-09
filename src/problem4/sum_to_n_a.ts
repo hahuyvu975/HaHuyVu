2ed8a6fa467bfafcf01252eaefda67df0f4dd121
unknown <hahuyvu975@gmail.com>
2024-04-09 17:49:30 +0700

    complexity way1

--- src/problem4/sum_to_n_a.ts
+++ src/problem4/sum_to_n_a.ts
@@ -1,27 +1,27 @@
-// Cách 1
-// Độ phức tạp: 
-// Độ phức tạp thời gian: O(num) - Chạy từ 1 đến num do đó độ phức tạp thời gian là tuyến tính theo n.
-// Độ phức tạp thời gian: O(1) - Hàm chỉ sử dụng một lượng không đổi bộ nhớ bổ sung bất kể giá trị của n.
+// Way 1
+// Complexity::
+// Time Complexity: O(num) - The function iterates from 1 to n, resulting in linear time complexity.
+// Space Complexity:  O(1) - The function uses constant space regardless of the input n.
 function sum_to_n_a_way_1(num: number): number {
-    let sum: number = 0;
-    for (let i = 1; i <= num; i++) {
-        sum += i;
-    };
+  let sum: number = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
 
-    return sum;
+  return sum;
 }
 
-// Cách 2
+// Way 2
 function sum_to_n_a_way_2(num: number): number {
-    return (num * (num + 1))/ 2
+  return (num * (num + 1)) / 2;
 }
 
-// Cách 3
+// Way 3
 function sum_to_n_a_way_3(num: number): number {
-    if(num === 0 ){
-        return 0;
-    };
-    return num + sum_to_n_a_way_3(num - 1);
+  if (num === 0) {
+    return 0;
+  }
+  return num + sum_to_n_a_way_3(num - 1);
 }
 
 const num: number = 5;
